74edae6e21b4526d7d2376efcf03e7eed5831ff3
王华林 <Hualin.Wang@geely.com>
2022-06-07 05:07:49 +0000

    修复UDP读取问题

--- udp/asyncUdpSocket.cpp
+++ udp/asyncUdpSocket.cpp
@@ -23,13 +23,11 @@ namespace net {
         Close();
     }
     void AsyncUdpSocket::Write(const std::string &data) {
-        int32_t nwrite =
-            UdpSocket::Write(GetNativeSocket(), data.data(), data.size(), GetINetHost());
+        int32_t nwrite = UdpSocket::Write(GetNativeSocket(), data.data(), data.size(), GetINetHost());
         // UDP发送失败返回-1，成功返回整个报文大小，没有中间值;没送发送缓冲区
         if (-1 == nwrite) {
-            std::cout << "EpollLoop::ProcessLoop HandleWrite error, errno: " << errno
-                      << ", " << ::strerror(errno) << " write bytes " << nwrite
-                      << std::endl;
+            std::cout << "EpollLoop::ProcessLoop HandleWrite error, errno: " << errno << ", " << ::strerror(errno) << " write bytes "
+                      << nwrite << std::endl;
         }
     }
     void AsyncUdpSocket::Read(READ_EVENT_CB cb) {
@@ -37,22 +35,25 @@ namespace net {
     }
 
     int32_t AsyncUdpSocket::HandleRead() {
-        std::string buffer(1500, 0);
-        INetHost remote;
-        auto bytes = UdpSocket::Read(GetNativeSocket(), const_cast<char *>(buffer.data()),
-                                     buffer.size(), remote);
-        if (0 >= bytes) {
-            return bytes;
+        int32_t nread = 0;
+        int32_t readSize = 0;
+        while (true) {
+            std::string buffer(1500, 0);
+            INetHost remote;
+            nread = UdpSocket::Read(GetNativeSocket(), const_cast<char *>(buffer.data()), buffer.size(), remote);
+            if (0 >= nread) {
+                break;
+            }
+            buffer.resize(nread);
+            buffer.shrink_to_fit();
+            SetHost(remote);
+            readSize += nread;
+            if (0 < nread && m_readCb) {
+                m_readCb(shared_from_this(), buffer);
+            }
         }
 
-        buffer.resize(bytes);
-        buffer.shrink_to_fit();
-        SetHost(remote);
-        if (m_readCb) {
-            m_readCb(shared_from_this(), buffer);
-        }
-
-        return bytes;
+        return nread;
     }
 
     int32_t AsyncUdpSocket::HandleWrite() {
